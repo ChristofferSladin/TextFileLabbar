d351293893c0cb387a0f2bb74d9e4d762bed9aea
Christoffer S <christoffer.sladin@hotmail.com>
2022-09-26 19:19:10 +0200

    Add project files.

---
+++ TextFileLabbar/ApplicationsTextFileLabbar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileLabbar
+{
+    internal class ApplicationsTextFileLabbar
+    {
+
+        public void Lab2()
+        {
+            using (var file = File.CreateText("ResultLab2.txt"))
+
+
+            {
+                foreach (var row in File.ReadLines("Lab2TextFile1.txt"))
+                {
+                    
+                    file.WriteLine(row);
+                }
+                    
+
+                foreach (var row in File.ReadLines("Lab2TextFile2.txt"))
+                {
+                    
+                    file.WriteLine(row);
+                }
+            }
+        }
+
+        public void Lab1()
+        {
+            var odd = true;
+
+            foreach (var row in File.ReadLines("Labb1.txt"))
+            {
+                if (odd)
+                Console.WriteLine(row);
+            }
+        }
+
+        public void Run()
+        {
+            //Lab1();
+            Lab2();
+
+        }
+    }
+}
---
+++ TextFileLabbar/Program.cs
@@ -0,0 +1,4 @@
+﻿using TextFileLabbar;
+
+var app = new ApplicationsTextFileLabbar();
+app.Run();
